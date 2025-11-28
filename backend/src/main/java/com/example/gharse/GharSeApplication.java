cd180206cb2a41f73cbac88aba8629ff5aaeaa60
Yash <79582883+yash-aryan@users.noreply.github.com>
2025-11-28 20:57:07 +0530

    Initial backend setup using Spring Boot

---
+++ backend/src/main/java/com/example/gharse/GharSeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gharse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GharSeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GharSeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/gharse/GharSeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gharse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GharSeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
